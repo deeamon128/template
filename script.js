aeabac674480c24fc00bc5c3d47bcfe1924eccbb
Andreea Bobariu <79876270+deeamon128@users.noreply.github.com>
2022-05-12 19:58:25 +0100

    dots change js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const productContainer = [...document.querySelectorAll(".product-container")];
 const nextBtn = [...document.querySelectorAll(".next-btn")];
+const dots = [...document.getElementsByClassName("dot")];
 
 productContainer.forEach((item, i) => {
   let containerDimension = item.getBoundingClientRect();
@@ -9,3 +10,23 @@ productContainer.forEach((item, i) => {
     item.scrollLeft += containerWidth;
   });
 });
+
+function currentSlide(n) {
+  var i;
+  const slides = document.getElementsByClassName("product-thumb");
+  const dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    n = 1;
+  } else if (n < 1) {
+    n = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  // slides[n - 1].style.display = "block";
+  dots[n - 1].className += " active";
+}
