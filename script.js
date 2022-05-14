e0e06c49f91800025ab96bf7c75da53225e3a5d3
Andreea Bobariu <79876270+deeamon128@users.noreply.github.com>
2022-05-14 14:23:49 +0100

    new update

--- script.js
+++ script.js
@@ -11,22 +11,43 @@ productContainer.forEach((item, i) => {
   });
 });
 
-function currentSlide(n) {
-  var i;
-  const slides = document.getElementsByClassName("product-thumb");
-  const dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    n = 1;
-  } else if (n < 1) {
-    n = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+// function currentSlide(n) {
+//   var i;
+//   const slides = document.getElementsByClassName("product-thumb");
+//   const dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {
+//     n = 1;
+//   } else if (n < 1) {
+//     n = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
 
-  slides[n - 1].style.display = "block";
-  dots[n - 1].className += " active";
-}
+//   slides[n - 1].style.display = "block";
+//   dots[n - 1].className += " active";
+// }
+// 0;
+
+// let slidePosition = 0;
+// const slides = document.getElementsByClassName(".product-card");
+// const totalSlides = slides.lenght;
+
+// document
+//   .getElementsByClassName(".next-btn")
+//   .addEventListener("click", function () {
+//     moveToNextSlide();
+//   });
+
+// function moveToNextSlide() {
+//   if (slidePosition === totalSlides - 1) {
+//     slidePosition = 0;
+//   } else {
+//     slidePosition++;
+//   }
+
+//   updateSlidePosition();
+// }
